0e4911ab332dbb6a92482c60fcdf80a50baab978
John Martinelli <johnnymartinelli@gmail.com>
2015-07-15 20:18:44 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# SECURITIplatform Configuration File
+
+# mongoDB connection settings
+
+mongoIP = "127.0.0.1"
+mongoPort = "27017"
+database = "databaseName"
+
+# output preference
+outputType = "json"
+
+# Your Google Apps API Key
+GoogleAPIKey = "GOOGLE_API_KEY_HERE"
